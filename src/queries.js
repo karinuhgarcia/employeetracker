d6c566e5055a3ca8b271340f3b162d62c6ac5678
Karina Garcia <karinag.000@gmail.com>
2025-04-09 00:56:02 -0700

    cleaning up

--- src/queries.js
+++ src/queries.js
@@ -33,6 +33,7 @@ export async function addEmployee(first_name, last_name, role_id, manager_id) {
   await pool.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)',
     [first_name, last_name, role_id, manager_id]);
 };
-export async function updateEmployee(employee_id, role_id) {
+export async function updateEmployeeRole(employee_id, role_id) {
   await pool.query('UPDATE employee SET role_id = $1 WHERE id = $2', [role_id, employee_id]);
 };
+
